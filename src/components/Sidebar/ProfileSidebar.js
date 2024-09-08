46bacc212facec83209bde1b3081fb3e885b1740
Reigne <monteronaelija@yahoo.com>
2024-09-08 23:46:18 +0800

    added color in button and href

--- src/components/Sidebar/ProfileSidebar.js
+++ src/components/Sidebar/ProfileSidebar.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation, Link, useParams } from "react-router-dom";
 import { Avatar, Button, Divider, Image } from "antd";
 import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
 } from "@headlessui/react";
-import { Link as RouterLink, useLocation } from "react-router-dom";
+// import { Link as RouterLink, useLocation } from "react-router-dom";
 import { TbLayoutDashboard } from "react-icons/tb";
 import { BsBoxSeam } from "react-icons/bs";
 import { TbCategory2 } from "react-icons/tb";
@@ -15,20 +15,22 @@ import { HiOutlinePresentationChartBar } from "react-icons/hi2";
 import { LiaTruckLoadingSolid } from "react-icons/lia";
 import { FaRegStar } from "react-icons/fa";
 import { PiUsersThree } from "react-icons/pi";
-import {
-  UserIcon,
-} from "@heroicons/react/16/solid";
+import { UserIcon } from "@heroicons/react/16/solid";
 import { FaRegUser } from "react-icons/fa6";
 import { SlUser } from "react-icons/sl";
+
 export default function ProfileSidebar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
 
   const { user, loading, isLogout } = useSelector((state) => state.auth);
 
-  useEffect((
+  useEffect(() => {}, []);
 
-  ) => {}, []);
+  const getCurrentPath = () => {
+    return location.pathname.split("/")[1];
+  };
 
   return (
     <div className="bg-zinc-100 sticky top-0 w-[20rem] p-6">
@@ -56,21 +58,45 @@ export default function ProfileSidebar() {
             <Divider />
 
             <div className="space-y-4">
-              <div className="hover:bg-green-500 hover:text-white py-2 px-3 rounded cursor-pointer w-full flex flex-1 flex-row justify-between items-center">
+              <a
+                className={`${
+                  getCurrentPath() === "profile"
+                    ? "bg-green-500 text-white"
+                    : ""
+                } hover:bg-green-500 hover:text-white py-2 px-3 rounded cursor-pointer w-full flex flex-1 flex-row justify-between items-center`}
+                href="/profile"
+              >
                 <div className="flex items-center gap-4">
-                  <SlUser color="#0f172a" size={20} />
+                  <SlUser
+                    color={`${
+                      getCurrentPath() === "profile" ? "white" : "#0f172a"
+                    }`}
+                    size={20}
+                  />
 
                   <span className="">Profile</span>
                 </div>
-              </div>
+              </a>
 
-              <div className="hover:bg-green-500 hover:text-white py-2 px-3 rounded cursor-pointer w-full flex flex-1 flex-row justify-between items-center">
+              <a
+                className={`${
+                  getCurrentPath() === "my-orders"
+                    ? "bg-green-500 text-white"
+                    : ""
+                } hover:bg-green-500 hover:text-white py-2 px-3 rounded cursor-pointer w-full flex flex-1 flex-row justify-between items-center`}
+                href="/my-orders"
+              >
                 <div className="flex items-center gap-4">
-                  <LiaTruckLoadingSolid color="#0f172a" size={20} />
+                  <LiaTruckLoadingSolid
+                    color={`${
+                      getCurrentPath() === "my-orders" ? "white" : "#0f172a"
+                    }`}
+                    size={20}
+                  />
 
                   <span className="">My Orders</span>
                 </div>
-              </div>
+              </a>
             </div>
 
             {/* <Divider /> */}
